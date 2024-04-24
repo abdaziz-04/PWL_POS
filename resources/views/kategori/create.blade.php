f2e9ef8a6edcd58a0b71c4bd1c9b86e8d49336bd
Abdul Aziz <abdulaziz@Abduls-MacBook-Air.local>
2024-04-25 02:54:00 +0700

    membuat view kategori

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -1,47 +1,46 @@
-@extends('layouts.app')
-
-@section('subtitle', 'welcome')
-@section('content_header_title', 'Category')
-@section('content_header_subtitle', 'Create')
-
+@extends('layouts.template')
 @section('content')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <div class="container">
         <div class="card card-primary">
             <div class="card-header">
                 <h3 class="card-title">Buat Kategori Baru</h3>
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
             </div>
-
-            <form action="../kategori" method="post">
+            <form action="/kategori" method="post">
+                @csrf
                 <div class="card-body">
                     <div class="form-group">
                         <label for="kodeKategori">Kode Kategori</label>
-                        <input type="text" name="kodeKategori" id="kodeKategori" class="form-control"
-                            placeholder="Masukkan Kode Kategori">
+                        <input type="text" name="kategori_kode" id="kodeKategori" placeholder="Kode Kategori"
+                            class="form-control
+                        @error('kategori_kode')
+                        is-invalid
+                        @enderror">
+
                         @error('kategori_kode')
                             <div class="alert alert-danger">{{ $message }}</div>
                         @enderror
                     </div>
                     <div class="form-group">
                         <label for="namaKategori">Nama Kategori</label>
-                        <input type="text" name="namaKategori" id="kodeKategori" placeholder="Masukkan Nama Kategori"
-                            class="form-control">
+                        <input type="text" class="form-control" id="namaKategori" name="kategori_nama"
+                            placeholder="Nama Kategori">
                     </div>
                 </div>
+
                 <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">
-                        Submit
-                    </button>
+                    <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </form>
+
         </div>
     </div>
 @endsection
--- resources/views/kategori/edit.blade.php
+++ resources/views/kategori/edit.blade.php
@@ -1,36 +1,33 @@
-@extends('layouts.app')
-
-@section('subtitle', 'welcome')
-@section('content_header_title', 'Kategori')
-@section('content_header_subtitle', 'Edit')
-
+@extends('layouts.template')
 @section('content')
     <div class="container">
         <div class="card card-primary">
             <div class="card-header">
-                <h3 class="card-title">Buat Kategori Baru</h3>
+                <h3 class="card-title">Edit kategori</h3>
             </div>
-            <form action="../{{ $data->kategori_id }}" method="post">
+
+            <form method="post" action="{{ $data->kategori_id }}">
+
                 {{ csrf_field() }}
                 {{ method_field('PUT') }}
+
                 <div class="card-body">
                     <div class="form-group">
                         <label for="kodeKategori">Kode Kategori</label>
-                        <input type="text" value="{{ $data->kategori_kode }}" name="kodeKategori" id="kodeKategori"
-                            class="form-control">
+                        <input type="text" class="form-control" id="kodeKategori" name="kodeKategori"
+                            value="{{ $data->kategori_kode }}">
                     </div>
                     <div class="form-group">
                         <label for="namaKategori">Nama Kategori</label>
-                        <input type="text" value="{{ $data->kategori_nama }}" name="namaKategori" id="namaKategori"
-                            class="form-control">
+                        <input type="text" class="form-control" id="namaKategori" name="namaKategori"
+                            value="{{ $data->kategori_nama }}">
                     </div>
                 </div>
 
                 <div class="card-footer">
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" class="btn btn-primary">Edit</button>
                 </div>
             </form>
-
         </div>
     </div>
 @endsection

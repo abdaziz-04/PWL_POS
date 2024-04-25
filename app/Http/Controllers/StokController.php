996671f94298a7fdd03f85224714e05bf0f65325
Abdul Aziz <abdulaziz@Abduls-MacBook-Air.local>
2024-04-25 09:23:29 +0700

    Controller stok barang

--- app/Http/Controllers/StokController.php
+++ app/Http/Controllers/StokController.php
@@ -2,63 +2,162 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\BarangModel;
+use App\Models\StokModel;
 use Illuminate\Http\Request;
+use DataTables;
 
 class StokController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $breadcrumb = (object) [
+            'title' => 'Daftar Stok',
+            'list' => ['Home', 'Stok']
+        ];
+
+        $page = (object) [
+            'title' => 'Daftar Stok yang terdaftar dalam sistem'
+        ];
+
+        $activeMenu = 'stok';
+        $stok = StokModel::all();
+        return view('stok.index', [
+            'breadcrumb' => $breadcrumb,
+            'page' => $page,
+            'stok' => $stok,
+            'activeMenu' => $activeMenu
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    public function list(Request $request)
+    {
+        $stok = StokModel::with(['user', 'barang'])->select('*');
+
+        if ($request->stok_id) {
+            $stok->where('stok_id', $request->stok_id);
+        }
+        return DataTables::of($stok)
+            ->addIndexColumn()
+            ->addColumn('aksi', function ($stok) {
+                $btn = '<a href="' . url('/stok/' . $stok->stok_id) . '" class="btn btn-info btn-sm">Detail</a> ';
+                $btn .= '<a href="' . url('/stok/' . $stok->stok_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
+                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/stok/' . $stok->stok_id) . '">' . csrf_field() . method_field('DELETE') .
+                    '<button type="submit" class="btn btn-danger btn-sm"
+                    onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button></form>';
+                return $btn;
+            })
+            ->rawColumns(['aksi'])
+            ->make(true);
+    }
     public function create()
     {
-        //
+        $breadcrumb = (object) [
+            'title' => 'Tambah Stok',
+            'list' => ['Home', 'stok', 'tambah']
+        ];
+        $page = (object) [
+            'title' => 'Tambah Stok Baru'
+        ];
+
+        $stok = StokModel::all();
+        $activeMenu = 'stok';
+        return view('stok.create', ['breadcrumb' => $breadcrumb, 'page' => $page, 'stok' => $stok, 'activeMenu' => $activeMenu]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
-        //
+
+        $request->validate([
+
+            'barang_id' => 'bail|required|max:255',
+            'user_id' => 'bail|required|max:255',
+            'stok_tanggal' => 'bail|required|max:255',
+            'stok_jumlah' => 'bail|required|max:255',
+
+        ]);
+
+        StokModel::create([
+            'barang_id' => $request->barang_id,
+            'user_id' => $request->user_id,
+            'stok_tanggal' => $request->stok_tanggal,
+            'stok_jumlah' => $request->stok_jumlah,
+        ]);
+
+        return redirect('/stok');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(string $id)
     {
-        //
+        $stok = StokModel::find($id);
+
+        $breadcrumb = (object) [
+            'title' => 'Edit stok',
+            'list' => ['Home', 'stok', 'Edit']
+        ];
+
+        $page = (object) [
+            'title' => 'Edit stok'
+
+        ];
+
+        $activeMenu = 'stok';
+        return view('stok.edit', ['breadcrumb' => $breadcrumb, 'page' => $page, 'stok' => $stok, 'activeMenu' => $activeMenu]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'barang_id' => 'bail|required|max:255',
+            'user_id' => 'bail|required|max:255',
+            'stok_tanggal' => 'bail|required|max:255',
+            'stok_jumlah' => 'bail|required|max:255',
+        ]);
+
+        StokModel::find($id)->update([
+            'barang_id' => $request->barang_id,
+            'user_id' => $request->user_id,
+            'stok_tanggal' => $request->stok_tanggal,
+            'stok_jumlah' => $request->stok_jumlah,
+        ]);
+
+        return redirect('/stok')->with('success', 'Data berhasil diubah');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
+    public function show(string $id)
+    {
+        $stok = StokModel::find($id);
+
+        $breadcrumb = (object) [
+            'title' => 'Detail stok',
+            'list' => ['Home', 'stok', 'Detail']
+        ];
+
+        $page = (object) [
+            'title' => 'Detail stok'
+        ];
+
+        $activeMenu = 'stok';
+
+        return view('stok.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'stok' => $stok, 'activeMenu' => $activeMenu]);
+    }
     public function destroy(string $id)
     {
-        //
+        $check = StokModel::find($id);
+        if (!$check) {
+            return redirect('/stok')->with('error', 'Data tidak ditemukan');
+        }
+
+        try {
+            StokModel::destroy($id);
+
+            return redirect('/stok')->with('seccess', 'Data berhasil dihapus');
+        } catch (\Illuminate\Database\QueryException $e) {
+
+            return redirect('/stok')->with('error', 'Data gagal dihapus karena masih terdapat tabel lain yang terkai dengan data ini');
+        }
     }
+
 }

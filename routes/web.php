1e0a5aaeba5b3f70e73f599e8b4120fd0de650b0
Abdul Aziz <abdulaziz@Abduls-MacBook-Air.local>
2024-04-25 09:23:45 +0700

    route dll

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\POSController;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\StokController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
@@ -23,26 +24,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/level', [LevelController::class, 'index']);
-// Route::get('/kategori', [KategoriController::class, 'index']);
-// Route::get('/user', [UserController::class, 'index']);
-// Route::get('/user/tambah', [UserController::class, 'tambah']);
-// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
-
-// Route::get('/kategori/create', [KategoriController::class, 'create']);
-// Route::post('/kategori', [KategoriController::class, 'store']);
-// Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit']);
-// Route::put('/kategori/{id}', [KategoriController::class, 'update']);
-// Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete']);
 
 Route::get('/', [WelcomeController::class, 'index']);
 

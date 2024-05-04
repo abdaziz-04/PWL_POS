387b3ee1943269ec228bb641e43d842622e477aa
Abdul Aziz <aziz@Abduls-MacBook-Air.local>
2024-05-04 09:25:22 +0700

    prak 4 - crud level

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,6 @@ Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// CRUD LEVEL
+Route::resource('levels', LevelController::class); // Saya menggunakan Route::resource agar mempersingkat penulisan kode

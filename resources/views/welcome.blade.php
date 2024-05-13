1c0c6d40e23a519cf8fe8ee836edbe1bdacbbcf1
Abdul Aziz <aziz@Abduls-MacBook-Air.local>
2024-05-13 21:33:09 +0700

    gabut - nambah gif

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,9 @@
             <h3 class="card-title">Halo, Apa Kabar!!!</h3>
             <div class="card-tools"></div>
         </div>
-        <div class="card-body">
+        <div class="card-body" style="position: relative;">
             Selamat Datang Semua, Ini adalah halaman utama dari aplikasi ini.
+            <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj6ilNnjR_VGoBm-H7-cB2N1UFR-8Di_s0wt-gYfJSBNFclUwpHbmChwOCfvmIvm4NtC3YLxILULxNurPs4eHBkFz6A2xhcab_Xw1iGeBuEuUfVFF7RbZKI_Ao9QlBzTsrGjft_yXhEtmt4/s1600-rw/gift+17.gif" alt="gif" style="position: absolute; bottom: 0; right: 0;">
         </div>
     </div>
 @endsection
